54ee443edc58da3183baff7e9f5da685a114ca34
anshusinha872 <anshusinha872@gmail.com>
2022-09-21 10:39:11 +0530

    intial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {ButtonModule} from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import {SliderModule} from 'primeng/slider';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    SliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
